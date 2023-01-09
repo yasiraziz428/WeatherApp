d288ed2413aecc2a9944b37dcf29977f2ab28057
Yasir Aziz <yasiraziz428@gmail.com>
2023-01-09 13:14:03 +0500

    v2

--- server.js
+++ server.js
@@ -3,17 +3,18 @@ const https = require("https");
 const bodyParser = require("body-parser");
 
 const app = express();
-app.use(express.static("public"));
+app.set("view engine", "ejs");
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
+  res.render("index");
 });
 
 app.post("/", (req, res) => {
   const apiKey = "45c85a44545192c79ae92b579cf6cc13";
-  const city = req.body.city_name;
+  const city = req.body.city_name.toUpperCase();
   const units = "metric";
   const url = `https://api.openweathermap.org/data/2.5/weather?appid=${apiKey}&q=${city}&units=${units}`;
 
@@ -21,17 +22,16 @@ app.post("/", (req, res) => {
     if (response.statusCode === 200) {
       response.on("data", (data) => {
         weatherData = JSON.parse(data);
-        const temp = weatherData.main.temp;
+        const temp = Math.round(weatherData.main.temp);
         const icon = weatherData.weather[0].icon;
         const icon_url = `http://openweathermap.org/img/wn/${icon}@2x.png`;
 
-        res.write(`<h1>The temperature in ${city} is ${temp} Celcius</h1>`);
-        res.write(
-          `<h4>The weather is currently ${weatherData.weather[0].description}.</h4>`
-        );
-        res.write(`<img src="${icon_url}" />`);
-
-        res.send();
+        res.render("home", {
+          city: city,
+          url: icon_url,
+          temp: temp,
+          desc: weatherData.weather[0].description,
+        });
       });
     } else {
       res.sendFile(__dirname + "/failure.html");
